ec592a161b29365f61719bd810210603ed174dbf
Ivan Sherstobitov <alliencool@mail.ru>
2014-10-09 00:14:56 +0400

    Draft non-working version of 57 task.

---
+++ task57.py
@@ -0,0 +1,19 @@
+
+class PyRational(object):
+
+    def __init__(self, numerator, denominator=1):
+
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def _gcd(self, a, b):
+        pass
+    
+    def __add__(self, rational):
+        pass
+
+def task57():
+    pass
+
+if __name__ == "__main__":
+    task57()
